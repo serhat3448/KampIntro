f27b2ae9e374e0526b5668e9da14a7e89527a370
serhat doğru <dogru.serhat@hotmail.com>
2021-01-27 17:16:59 +0300

    5. Gun Sonu

---
+++ OOP2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Serhat Doğru
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Serhat";
+            musteri1.Soyadi = "Doğru";
+            musteri1.TcNo = "12345678910";
+
+
+            //Kodlama.io
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "123456789";
+
+
+            //SOLID içerisinden L
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+            musteriManager.Ekle(musteri4);
+
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManagar();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager esnafKrediManager = new EsnafKredisiManager();
+
+            ILoggerService databasLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            ILoggerService smsLoggerService = new SmsLoggerService();
+
+            List<ILoggerService> loggers = new List<ILoggerService>() { databasLoggerService, smsLoggerService };
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(esnafKrediManager, loggers);
+
+            
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, tasitKrediManager };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+        }
+    }
+}
