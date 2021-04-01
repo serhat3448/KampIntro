7bd09b2f7d98535f6644f9b9844e433e763c95fa
serhat doğru <dogru.serhat@hotmail.com>
2021-04-01 21:42:46 +0300

    4. Gün 5. Ödev

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Key, Value>
+    {
+        (Key, Value)[] keyValues;
+
+        public MyDictionary()
+        {
+            keyValues = new (Key, Value)[0];
+        }
+
+        public void Add(Key No, Value Name)
+        {
+            (Key, Value)[] tempArray = keyValues;
+
+            keyValues = new (Key, Value)[keyValues.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                keyValues[i] = tempArray[i];
+            }
+
+            keyValues[keyValues.Length - 1] = (No, Name);
+        }
+
+
+        public void Listele()
+        {
+            for (int i = 0; i < keyValues.Length; i++)
+            {
+                Console.WriteLine("Adı ve Numarası" + keyValues[i]);
+            }
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using MyDictionary;
+using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
+            dictionary.Add(1, "Serhat");
+            dictionary.Add(105, "Serkan");
+            dictionary.Add(88, "Selda");
+
+            dictionary.Listele();
+        }
+    }
+}
